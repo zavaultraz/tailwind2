7c97a87bee85a05b093c5af2b24018974057b52a
zavaultraz <zavazaneta@gmail.com>
2024-02-07 14:43:55 +0700

    woew

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-  "./node_modules/flowbite/**/*.js"
-],
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {
-      'Kanit' : ['Kanit']
+      animation: {
+        marquee: 'marquee 20s linear infinite',
+        marquee2: 'marquee2 20s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': {
+            transform: 'translateX(0%)'
+          },
+          '100%': {
+            transform: 'translateX(-100%)'
+          },
+        },
+        marquee2: {
+          '0%': {
+            transform: 'translateX(100%)'
+          },
+          '100%': {
+            transform: 'translateX(0%)'
+          },
+        },
+      },
     },
   },
   plugins: [
     require('flowbite/plugin')
   ],
 }
-
